4487f6bbd5ca1ba7de8306ac9d936565a05be12a
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2026-01-12 18:15:42 -0700

    home, about and product pages added, links working

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
+// Imports
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
+// Startup express server
 const app = express();
 
+// Important vars
 const name = process.env.NAME;
 const otherName = process.env.OTHERNAME;
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const NODE_ENV = process.env.NODE_ENV || 'production';
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+// Serve static, public routes
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Declare routes
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
-app.get('/new-route', (req, res) => {
-    res.send(`Hello, ${otherName}`);
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
 });
 
-const PORT = 3000;
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
+// Start server and listen
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
