1cca1b657b5d0c8f93eacb117ce8a5c8da8879f7
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2026-01-28 19:11:07 -0700

    start the facutly details page and get links working

--- src/controllers/faculty/faculty.js
+++ src/controllers/faculty/faculty.js
@@ -1,11 +1,35 @@
-import { getSortedFaculty } from "../../models/faculty/faculty.js";
+import { getFacultyById, getSortedFaculty } from "../../models/faculty/faculty.js";
 
 export const facultyListPage = (req, res) => {
 
     const faculty = getSortedFaculty();
+    console.log('faculty', faculty);
 
     res.render('faculty/list', { title: 'Faculty', faculty });
 };
 
 export const facultyDetailPage = (req, res, next) => {
+
+    const facultyId = req.params.facultyId;
+
+    const faculty = getFacultyById(facultyId);
+
+    if (!faculty) {
+
+        const err = new Error(`Course ${facultyId} not found`);
+
+        err.status = 404;
+
+        return next(err);
+
+    }
+
+    res.render('faculty/detail', {
+
+        title: `${faculty.name} - ${faculty.department}`,
+
+        faculty
+
+    });
+
 };
